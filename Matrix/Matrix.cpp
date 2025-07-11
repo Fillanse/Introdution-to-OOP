d651146585c24f5da29a2001b524d1b0e8bec116
Fillanse <eligar728@gmail.com>
2025-07-11 23:37:48 +0500

    Done: Matrix constructors

--- Matrix/Matrix.cpp
+++ Matrix/Matrix.cpp
@@ -7,24 +7,43 @@ using namespace std;
 class Matrix
 {
 public:
-    Matrix(size_t rows, size_t cols) : _rows(rows), _cols(cols) { initialize(_rows, _cols); }
-    Matrix() : Matrix(2, 2) {}
-    Matrix(const Matrix& other) {
-        _rows = other._rows;
-        _cols = other._cols;
-        initialize(_rows, _cols);
-        for (size_t i = 0; i < _rows; i++) {
-            for (size_t j = 0; j < _cols; j++) {
-                _matrix[i][j] = other._matrix[i][j];
-            }
-        }
-    }
-    ~Matrix() { clear(); }
+	Matrix(size_t rows, size_t cols) : _rows(rows), _cols(cols) { initialize(_rows, _cols); }
+	template<size_t rows, size_t cols>
+	Matrix(const int (&arr)[rows][cols]) : Matrix(rows, cols) { // type mismatch for initialize(**,int,int)?
+		initialize(_rows, _cols);
+		for (size_t i = 0; i < _rows; i++) {
+			for (size_t j = 0; j < _cols; j++) {
+				_matrix[i][j] = arr[i][j];
+			}
+		}
+	}
+	Matrix() : Matrix(2, 2) {}
+	Matrix(initializer_list<initializer_list<int>> l) : _rows(l.size()), _cols(l.begin()->size()){
+		initialize(_rows, _cols);
+		size_t i = 0;
+		for (auto& row : l) {
+			size_t j = 0;
+			for (auto& elem : row) {
+				_matrix[i][j++] = elem;
+			}
+			i++;
+		}
+	}
+	Matrix(const Matrix& other) : Matrix(other._rows, other._cols) { initialize(other._matrix, _rows, _cols); }
+	Matrix(Matrix&& other) {
+		_rows = other._rows;
+		_cols = other._cols;
+		_matrix = other._matrix;
+		other._rows = 0;
+		other._cols = 0;
+		other._matrix = nullptr;
+	}
+	~Matrix() { clear(); }
 
-    int get_rows() const { return _rows; }
-    int get_cols() const { return _cols; }
-
-    Matrix& operator=(const Matrix& other) {
+	int get_rows() const { return _rows; }
+	int get_cols() const { return _cols; }
+	
+	Matrix& operator=(const Matrix& other) {
 		if (this == &other) return *this;
 		clear();
 		_rows = other._rows;
@@ -35,235 +54,290 @@ public:
 				_matrix[i][j] = other._matrix[i][j];
 			}
 		}
-    }
-    Matrix operator+(const Matrix& other) const {
+		return *this;
+	}
+	Matrix& operator=(Matrix&& other) {
+		if (this != &other) {
+			clear();
+			_rows = other._rows;
+			_cols = other._cols;
+			_matrix = other._matrix;
+			other._rows = 0;
+			other._cols = 0;
+			other._matrix = nullptr;
+		}
+		return *this;
+	}
+	Matrix operator+(const Matrix& other) const {
 		if (_rows != other._rows || _cols != other._cols) return Matrix(0, 0);
 
 		Matrix temp(_rows, _cols);
 		for (size_t i = 0; i < _rows; i++) {
 			for (size_t j = 0; j < _cols; j++) {
-			    temp[i][j] = _matrix[i][j] + other[i][j];
-            }
+				temp[i][j] = _matrix[i][j] + other[i][j];
+			}
 		}
 		return temp;
 	}
-    Matrix operator-(const Matrix& other) const {
-        if (_rows != other._rows || _cols != other._cols) return Matrix(0, 0);
+	Matrix operator-(const Matrix& other) const {
+		if (_rows != other._rows || _cols != other._cols) return Matrix(0, 0);
 
-        Matrix temp(_rows, _cols);
-        for (size_t i = 0; i < _rows; i++) {
-            for (size_t j = 0; j < _cols; j++) {
+		Matrix temp(_rows, _cols);
+		for (size_t i = 0; i < _rows; i++) {
+			for (size_t j = 0; j < _cols; j++) {
 
-                temp[i][j] = _matrix[i][j] - other[i][j];
-            }
-        }
-        return temp;
-    }
-    Matrix operator-() const {
-        Matrix temp(_rows, _cols);
-        for (size_t i = 0; i < _rows; i++) {
-            for (size_t j = 0; j < _cols; j++) {
-                temp[i][j] = -_matrix[i][j];
-            }
-        }
-        return temp;
-    }
-    Matrix operator*(const Matrix& other) const {
-        if (_cols != other._rows) return Matrix(0, 0);
+				temp[i][j] = _matrix[i][j] - other[i][j];
+			}
+		}
+		return temp;
+	}
+	Matrix operator-() const {
+		Matrix temp(_rows, _cols);
+		for (size_t i = 0; i < _rows; i++) {
+			for (size_t j = 0; j < _cols; j++) {
+				temp[i][j] = -_matrix[i][j];
+			}
+		}
+		return temp;
+	}
+	Matrix operator*(const Matrix& other) const {
+		if (_cols != other._rows) return Matrix(0, 0);
 
-        Matrix temp(_rows, other._cols);
-        for (size_t i = 0; i < _rows; i++) {
-            for (size_t j = 0; j < other._cols; j++) {
-                for (size_t k = 0; k < _cols; k++) {
-                    temp[i][j] += _matrix[i][k] * other[k][j];
-                }
-            }
-        }
-        return temp;
-    }
-    Matrix operator*(int value) const {
-        Matrix temp(_rows, _cols);
-        for (size_t i = 0; i < _rows; i++) {
-            for (size_t j = 0; j < _cols; j++) {
-                temp[i][j] = _matrix[i][j] * value;
-            }
-        }
-        return temp;
-    }
-    Matrix& operator+=(const Matrix& other) {
-        *this = *this + other;
-        return *this;
-    }
-    Matrix& operator-=(const Matrix& other) {
-        *this = *this - other;
-        return *this;
-    }
-    Matrix& operator*=(const Matrix& other) {
-        *this = *this * other;
-        return *this;
-    }
-    int* operator[](size_t index) {
-        return _matrix[index];
-    }
-    const int* operator[](size_t index) const {
-        return _matrix[index];
-    }
+		Matrix temp(_rows, other._cols);
+		for (size_t i = 0; i < _rows; i++) {
+			for (size_t j = 0; j < other._cols; j++) {
+				for (size_t k = 0; k < _cols; k++) {
+					temp[i][j] += _matrix[i][k] * other[k][j];
+				}
+			}
+		}
+		return temp;
+	}
+	Matrix operator*(int value) const {
+		Matrix temp(_rows, _cols);
+		for (size_t i = 0; i < _rows; i++) {
+			for (size_t j = 0; j < _cols; j++) {
+				temp[i][j] = _matrix[i][j] * value;
+			}
+		}
+		return temp;
+	}
+	Matrix& operator+=(const Matrix& other) {
+		*this = *this + other;
+		return *this;
+	}
+	Matrix& operator-=(const Matrix& other) {
+		*this = *this - other;
+		return *this;
+	}
+	Matrix& operator*=(const Matrix& other) {
+		*this = *this * other;
+		return *this;
+	}
+	int* operator[](size_t index) {
+		return _matrix[index];
+	}
+	const int* operator[](size_t index) const {
+		return _matrix[index];
+	}
 
-    int determinant() const {
-        if (_rows != _cols || _rows == 0) return 0;
-        if (_rows == 1) return _matrix[0][0];
-        if (_rows == 2) return _matrix[0][0] * _matrix[1][1] - _matrix[0][1] * _matrix[1][0];
+	int determinant() const {
+		if (_rows != _cols || _rows == 0) return 0;
+		if (_rows == 1) return _matrix[0][0];
+		if (_rows == 2) return _matrix[0][0] * _matrix[1][1] - _matrix[0][1] * _matrix[1][0];
 
-        int det = 0;
-        int sign = 1;
-        for (size_t j = 0; j < _cols; j++) {
-            int minor_det = minor(0, j);
-            det += sign * _matrix[0][j] * minor_det;
-            sign = -sign;
-        }
-        return det;
-    }
+		int det = 0;
+		int sign = 1;
+		for (size_t j = 0; j < _cols; j++) {
+			int minor_det = minor(0, j);
+			det += sign * _matrix[0][j] * minor_det;
+			sign = -sign;
+		}
+		return det;
+	}
 
-    int minor(size_t row, size_t col) const {
-        if (_rows <= 1 || _cols <= 1) return 0;
+	int minor(size_t row, size_t col) const {
+		if (_rows <= 1 || _cols <= 1) return 0;
 
-        Matrix temp(_rows - 1, _cols - 1);
-        size_t r = 0;
-        for (size_t i = 0; i < _rows; i++) {
-            if (i == row) continue;
-            size_t c = 0;
-            for (size_t j = 0; j < _cols; j++) {
-                if (j == col) continue;
-                temp[r][c] = _matrix[i][j];
-                c++;
-            }
-            r++;
-        }
-        return temp.determinant();
-    }
+		Matrix temp(_rows - 1, _cols - 1);
+		size_t r = 0;
+		for (size_t i = 0; i < _rows; i++) {
+			if (i == row) continue;
+			size_t c = 0;
+			for (size_t j = 0; j < _cols; j++) {
+				if (j == col) continue;
+				temp[r][c] = _matrix[i][j];
+				c++;
+			}
+			r++;
+		}
+		return temp.determinant();
+	}
 
-    Matrix inverse() const {
-        if (_rows != _cols || _rows == 0)return Matrix(0, 0);
+	Matrix inverse() const {
+		if (_rows != _cols || _rows == 0)return Matrix(0, 0);
 
-        int det = determinant();
+		int det = determinant();
 
-        if (det == 0) return Matrix(0, 0);
+		if (det == 0) return Matrix(0, 0);
 
-        Matrix cofactor(_rows, _cols);
-        for (size_t i = 0; i < _rows; i++) {
-            for (size_t j = 0; j < _cols; j++) {
-                cofactor[i][j] = ((i + j) % 2 ? -1 : 1) * minor(i, j);
-            }
-        }
+		Matrix cofactor(_rows, _cols);
+		for (size_t i = 0; i < _rows; i++) {
+			for (size_t j = 0; j < _cols; j++) {
+				cofactor[i][j] = ((i + j) % 2 ? -1 : 1) * minor(i, j);
+			}
+		}
 
-        Matrix attached = cofactor.transposed();
-        Matrix result(_rows, _cols);
-        for (size_t i = 0; i < _rows; i++) {
-            for (size_t j = 0; j < _cols; j++) {
-                result[i][j] = attached[i][j] / det;
-            }
-        }
-        return result;
-    }
+		Matrix attached = cofactor.transposed();
+		Matrix result(_rows, _cols);
+		for (size_t i = 0; i < _rows; i++) {
+			for (size_t j = 0; j < _cols; j++) {
+				result[i][j] = attached[i][j] / det;
+			}
+		}
+		return result;
+	}
 
-    Matrix transposed() const {
-        Matrix temp(_cols, _rows);
-        for (size_t i = 0; i < _rows; i++) {
-            for (size_t j = 0; j < _cols; j++) {
-                temp[j][i] = _matrix[i][j];
-            }
-        }
-        return temp;
-    }
+	Matrix transposed() const {
+		Matrix temp(_cols, _rows);
+		for (size_t i = 0; i < _rows; i++) {
+			for (size_t j = 0; j < _cols; j++) {
+				temp[j][i] = _matrix[i][j];
+			}
+		}
+		return temp;
+	}
 
-    void random() {
-        for (size_t i = 0; i < _rows; i++) {
-            for (size_t j = 0; j < _cols; j++) {
-                _matrix[i][j] = rand() % 10;
-            }
-        }
-    }
-    void print() const {
-        for (size_t i = 0; i < _rows; i++) {
-            for (size_t j = 0; j < _cols; j++) {
-                cout << _matrix[i][j] << " ";
-            }
-            cout << endl;
-        }
-        cout << endl;
-    }
+	void random() {
+		for (size_t i = 0; i < _rows; i++) {
+			for (size_t j = 0; j < _cols; j++) {
+				_matrix[i][j] = rand() % 10;
+			}
+		}
+	}
+	void print() const {
+		for (size_t i = 0; i < _rows; i++) {
+			for (size_t j = 0; j < _cols; j++) {
+				cout << _matrix[i][j] << " ";
+			}
+			cout << endl;
+		}
+		cout << endl;
+	}
 private:
-    void clear() {
-        if (_matrix) {
-            for (size_t i = 0; i < _rows; i++) {
-                delete[] _matrix[i];
-            }
-            delete[] _matrix;
-            _matrix = nullptr;
-        }
-    }
-    void initialize(size_t rows, size_t cols) {
-        _matrix = new int* [rows];
-        for (size_t i = 0; i < rows; i++) {
-            _matrix[i] = new int[cols];
-            for (size_t j = 0; j < cols; j++) {
-                _matrix[i][j] = {};
-            }
-        }
-    }
+	void clear() {
+		if (_matrix) {
+			for (size_t i = 0; i < _rows; i++) {
+				delete[] _matrix[i];
+			}
+			delete[] _matrix;
+			_matrix = nullptr;
+		}
+	}
+	void initialize(size_t rows, size_t cols) {
+		_matrix = new int* [rows];
+		for (size_t i = 0; i < rows; i++) {
+			_matrix[i] = new int[cols];
+			for (size_t j = 0; j < cols; j++) {
+				_matrix[i][j] = {};
+			}
+		}
+	}
+	void initialize(int** arr, size_t rows, size_t cols) {
+		_matrix = new int* [rows];
+		for (size_t i = 0; i < rows; i++) {
+			_matrix[i] = new int[cols];
+			for (size_t j = 0; j < cols; j++) {
+				_matrix[i][j] = arr[i][j];
+			}
+		}
+	}
 
-    size_t _cols;
-    size_t _rows;
-    int** _matrix = nullptr;
+	size_t _cols;
+	size_t _rows;
+	int** _matrix = nullptr;
 };
 ostream& operator<<(ostream& os, const Matrix& obj) {
-    for (int i = 0; i < obj.get_rows(); i++) {
-        for (int j = 0; j < obj.get_cols(); j++) {
-            os << obj[i][j] << " ";
-        }
-        os << endl;
-    }
-    os << endl;
-    return os;
+	for (int i = 0; i < obj.get_rows(); i++) {
+		for (int j = 0; j < obj.get_cols(); j++) {
+			os << obj[i][j] << " ";
+		}
+		os << endl;
+	}
+	os << endl;
+	return os;
 }
 
-int main()
-{
-    srand((unsigned)time(0));
-    Matrix A;
-    A.random();
-    cout << "A" << endl;
-    cout << A;
-    cout << delimiter;
+void main() {
+	srand((unsigned)time(0));
 
-    Matrix B;
-    B.random();
-    cout << "B" << endl;
-    cout << B;
-    cout << delimiter;
+	Matrix m1;
+	m1.print();
+	cout << delimiter;
 
-    cout << "A + B" << endl;
-    Matrix C = A + B;
-    C.print();
-    cout << delimiter;
+	Matrix m2 = { {1, 2, 3}, {4, 5, 6}, {7, 8, 9} };
+	cout << "m2:" << endl << m2;
+	cout << "Determinant: " << m2.determinant() << endl;
+	cout << delimiter;
 
-    cout << "A - B" << endl;
-    (A - B).print();
-    cout << delimiter;
+	Matrix A(3, 3);
+	A.random();
+	Matrix B(3, 3);
+	B.random();
 
-    cout << "A * B" << endl;
-    (A * B).print();
-    cout << delimiter;
+	cout << "Matrix A:" << endl << A;
+	cout << "Matrix B:" << endl << B;
+	cout << delimiter;
 
-    cout << "A * 2" << endl;
-    (A * 2).print();
-    cout << delimiter;
+	cout << "A + B:" << endl << (A + B);
+	cout << "A - B:" << endl << (A - B);
+	cout << "A * B:" << endl << (A * B);
+	cout << "A * 5:" << endl << (A * 5);
+	cout << "-A:" << endl << (-A);
+	cout << delimiter;
 
-    cout << "Determinant of A: " << A.determinant() << endl;
-    cout << delimiter;
+	Matrix C = A;
+	C += B;
+	cout << "C += B:" << endl << C;
 
-    cout << "Inverse of A:" << endl;
-    Matrix invA = A.inverse();
-    invA.print();
-    cout << delimiter;
+	C = A;
+	C -= B;
+	cout << "C -= B:" << endl << C;
+
+	C = A;
+	C *= B;
+	cout << "C *= B:" << endl << C;
+	cout << delimiter;
+
+	cout << "Transpose of A:" << endl << A.transposed();
+	cout << "Determinant of A: " << A.determinant() << endl;
+	cout << "Inverse of A:" << endl << A.inverse();
+	cout << "A * inv(A):" << endl << (A * A.inverse());
+	cout << delimiter;
+
+	Matrix empty(0, 0);
+	cout << "Empty matrix:" << endl << empty;
+	cout << "Determinant of empty: " << empty.determinant() << endl;
+
+	Matrix nonSquare(2, 3);
+	nonSquare.random();
+	cout << "Non-square matrix (2x3):" << endl << nonSquare;
+	cout << "Attempting inverse (should fail):" << endl << nonSquare.inverse();
+	cout << delimiter;
+
+	Matrix copy = A;
+	cout << "Copy of A:" << endl << copy;
+
+	Matrix moved = std::move(A);
+	cout << "Moved from A:" << endl << moved;
+	cout << "Original A (should be empty):" << endl << A;
+	cout << delimiter;
+
+	int arr[2][2] = { {1, 2}, {3, 4} };
+	Matrix fromArr(arr);
+	cout << "From array:" << endl << fromArr;
+	cout << "Determinant: " << fromArr.determinant() << endl;
+	cout << "Inverse:" << endl << fromArr.inverse();
+	cout << delimiter;
+
 }
---
+++ String_SPU411/main.cpp
@@ -0,0 +1,128 @@
+#include <Windows.h>
+#include "String.h"
+#include <iostream>
+using std::cin;
+using std::cout;
+using std::endl;
+
+#define delimiter "\n------------------------------------\n"
+
+#define DEBUG
+
+
+
+//#define CONSTRUCTORS_CHECK
+#define OPERATOR_PLUS
+//#define ISTREAM_OPERATOR
+//#define PERFORMANCE_TEST
+//#define CALLING_CONSTRUCTORS
+
+void main()
+{
+	setlocale(LC_ALL, "");
+
+#ifdef CONSTRUCTORS_CHECK
+	String str1(5);	//explicit-конструктор нельзя вызвать оператором '=', но всегда можно вызвать при помощи круглых скобок
+	str1.info();
+	cout << str1 << endl;
+
+	String str2 = "Hello";
+	str2 = str2;
+	cout << str2 << endl;
+
+	String str3 = str2;		//CopyConstructor
+	cout << str3 << endl;
+
+	String str4;
+	str4 = str3;
+	cout << str4 << endl;
+#endif // CONSTRUCTORS_CHECK
+
+#ifdef OPERATOR_PLUS
+	String str1 = "Hello";
+	String str2 = "World";
+	cout << delimiter << endl;
+	String str3;
+	str3 = str1 + str2;
+	cout << delimiter << endl;
+	cout << str3 << endl;
+	cout << delimiter << endl;
+	String str4 = str3;
+	cout << str4 << endl;
+	cout << delimiter << endl;
+	//MoveSemantic
+#endif // OPERATOR_PLUS
+
+#ifdef ISTREAM_OPERATOR
+	String str;
+	cout << "Введите строку: ";
+	SetConsoleCP(1251);
+	//cin >> str;
+	//cin.getline(str.get_str(), str.get_size());
+	getline(cin, str);
+	SetConsoleCP(866);
+	cout << str << endl;
+#endif // ISTREAM_OPERATOR
+
+#ifdef PERFORMANCE_TEST
+	String text = R"(When a return statement contains an expression of non-primitive type, its execution copies the expression result into the return slot of the calling function. The compiler invokes the copy or move constructor of the returned type. Then, as the function is exited, destructors for function-local variables are called, which includes any variables named in the expression.
+		The C++ standard allows(but doesn't require) the compiler to optionally construct the returned object directly in the return slot of the calling function. This construction skips (or elides) the copy or move constructor executed as part of the return statement. Unlike most other optimizations, this transformation is allowed to have an observable effect on the program's output.Namely, the copy or move constructor and associated destructor are called one less time.The standard still requires that the named returned variable has a defined copy or move constructor, even if the compiler elides the constructor in all cases.
+			In versions before Visual Studio 2022 version 17.4, when optimizations are disabled(such as under / Od or in functions marked #pragma optimize("", off)) the compiler only performs mandatory copy and move elision.Under / O2, the older compilers perform optional copy or move elision on return of a named variable in an optimized function when all of these conditions are met : it has no loops or exception handling, it doesn't return multiple symbols with overlapping lifetimes, the type's copy or move constructor doesn't have default arguments.
+			Visual Studio 2022 version 17.4 increases the number of places where the compiler does optional copy or move elisions under / Zc : nrvo, whether enabled explicitly, or automatically by using the / O2, / permissive - , or /std : c++20 or later options.Under / Zc : nrvo, the compiler performs optional copy or move elision on return of a named variable for any function when : it has no loops or exception handling(as before); it returns the variable from a loop; it has exception handling; the returned type's copy or move constructor has default arguments. Optional copy or move elisions are never done when /Zc:nrvo- is applied, or when the function returns multiple symbols with overlapping lifetimes, or for a throw of a named variable.)";
+	cout << text.get_size() << endl;
+
+	const int SIZE = 100000;
+	String arr[SIZE]{};
+	String get_text(const String & other);
+	for (int i = 0; i < SIZE; i++)
+	{
+		arr[i] = get_text(text);
+	}
+	cout << "DONE" << endl;
+#endif // PERFORMANCE_TEST
+
+#ifdef CALLING_CONSTRUCTORS
+	String str1;		//Default constructor
+	str1.info();
+
+	String str2(8);		//Single-argument constructor ('int') 8 - размер строки в Байтах;
+	str2.info();
+
+	String str3 = "Hello";	//Single-argumentconstructor ('const char*');
+	str3.info();
+	cout << typeid("Hello").name() << endl;
+
+	String str4();		//(Default constrcutor) Здесь НЕ вызывается никакой конструктор,
+	//и не создается никакой объект, здесь происходит объявление функции str4(),
+	//которая ничего НЕ принимает, и озвращает объект типа 'String'.
+//Пустые () НЕ делают явный вызов Default-конструктора.
+//str4.
+
+//Если нужно явно вызвать DefaultConstructor, это можно сделать {}
+	String str5(8);		//Создается строка длиной 8 Байт
+	String str6{ 8 };	//Создается строка длиной 8 Байт, т.е., {} вызывают конструктор.
+	String str7{};		//Явный вызов конструктора по умолчанию
+	//		!!!		{} СЛЕДУЕТ ИСПОЛЬЗОВАТЬ С ОСТОРОЖНОСТЬЮ		!!!
+
+	String str9 = str3;	//CopyConstructor
+	str9.info();
+#endif // CALLING_CONSTRUCTORS
+
+}
+
+String get_text(const String& other)
+{
+	String local = other;
+	return local;
+}
+
+/*
+---------------------------------
+C++11 MoveSemantic:
+MoveConstructor - Конструктор переноса;
+MoveAssignment  - Переносящий оператор присваивания;
+Move-методы принимают r-value reference: Class&&
+CopyConstructor,CopyAssignment - DeepCopy		(Побитовое копирование);
+MoveConstrcutor,MoveAssignment - ShallowCopy	(Поверхностное копирование);
+---------------------------------
+*/
